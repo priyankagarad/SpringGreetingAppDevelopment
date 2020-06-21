87f481ca01ea292b7eac8f06d13ada4e0ca0d270
priyankagarad <garadpriyanka1995@gmail.com>
2020-06-21 16:55:34 +0530

    ADDED: Configration Project

---
+++ src/main/java/com/bl/demo/FirstSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.bl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringAppApplication.class, args);
+    }
+
+}
